142a4710a935f7a2aecf2cd0f21fc49c420aa632
Tamik <hello@Tamik.ru>
2017-08-06 16:49:59 +0300

    :recycle: Подключен React Router

--- src/js/components/Application.js
+++ src/js/components/Application.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route, Link, Redirect } from 'react-router-dom';
+import FontIcon from 'material-ui/FontIcon';
+import { BottomNavigation, BottomNavigationItem } from 'material-ui/BottomNavigation';
 
 class Application extends Component {
+  state = {
+    selectedIndex: 0,
+    redirect: false,
+    route: '/',
+  }
+  select(index, route) {
+    this.setState({
+      selectedIndex: index,
+      redirect: true,
+      route: route,
+    });
+  }
   render() {
-    return(
-      <div>Hello, from main Application Component</div>
+    return (
+      <Router>
+        <div>
+          <div>
+            <Route exact path='/' component={Main} />
+            <Route path='/favorites' component={Favorites} />
+            <Route path='/radar' component={Radar} />
+          </div>
+          <div>
+            <BottomNavigation selectedIndex={this.state.selectedIndex}>
+               <Link to='/' replace> 
+                <BottomNavigationItem label='1' icon={eventsIcon} onTouchTap={() => this.select(0, '/')} />
+               </Link> 
+               <Link to='/favorites' replace> 
+                <BottomNavigationItem label='2' icon={favoritesIcon} onTouchTap={() => this.select(1, '/favorites')} />
+               </Link> 
+               <Link to='/radar' replace> 
+                <BottomNavigationItem label='3' icon={radarIcon} onTouchTap={() => this.select(2, '/radar')} />
+               </Link> 
+            </BottomNavigation>
+          </div>
+        </div>
+      </Router>
     );
   }
 }

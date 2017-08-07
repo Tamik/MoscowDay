0667be12849c99e7976c45757f7e057cf5b789ae
Tamik <hello@Tamik.ru>
2017-08-07 18:50:34 +0300

    :recycle: Добавлен компонент Radar

---
+++ src/js/components/pages/Radar/Radar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+import AppBar from 'material-ui/AppBar'
+
+const Radar = () => (
+  <div>
+    <AppBar title='Радар' showMenuIconButton={false} />
+  </div>
+)
+
+export default Radar

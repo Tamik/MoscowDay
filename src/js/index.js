fe72bdf874630a89e1cbcccaad186c4b84999447
Tamik <hello@Tamik.ru>
2017-08-07 18:50:04 +0300

    :recycle: Рефакторинг точки входа
    Да, опять

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { injectGlobal } from 'styled-components';
-import { globalStyle } from 'styles';
-import { AppContainer } from 'react-hot-loader';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Application from 'components/Application';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import { injectGlobal } from 'styled-components'
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+
+import { globalStyle } from 'styles'
+import Application from 'components/Application'
 
 function renderApp(Component) {
-  injectTapEventPlugin();
+  injectTapEventPlugin()
   render(
     <AppContainer>
       <MuiThemeProvider>
@@ -16,19 +18,19 @@ function renderApp(Component) {
       </MuiThemeProvider>
     </AppContainer>,
     document.getElementById('react-root')
-  );
+  )
 }
 
 function onDeviceReady() {
-  injectGlobal`${globalStyle}`;
-  renderApp();
+  injectGlobal`${globalStyle}`
+  renderApp()
 
   if (module.hot) {
     module.hot.accept('components/Application', () => {
-      const nextApplication = require('components/Application').default;
-      renderApp(nextApplication);
-    });
+      const nextApplication = require('components/Application').default
+      renderApp(nextApplication)
+    })
   }
 }
 
-document.addEventListener('deviceready', onDeviceReady);
+document.addEventListener('deviceready', onDeviceReady)

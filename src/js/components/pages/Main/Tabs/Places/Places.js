7715851ec71226bbae409fd924897f0f0f4c59c6
Tamik <hello@Tamik.ru>
2017-08-08 04:58:54 +0300

    :recycle: Добавлена поддержка модальных окон в Places компоненте

--- src/js/components/pages/Main/Tabs/Places/Places.js
+++ src/js/components/pages/Main/Tabs/Places/Places.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
+import ReactModal from 'react-modal'
 
 import { GridList, GridTile } from 'material-ui/GridList'
+import AppBar from 'material-ui/AppBar'
+import IconButton from 'material-ui/IconButton'
+import NavigationClose from 'material-ui/svg-icons/navigation/close'
+
+import PlacesList from '../../../../modals/PlacesList'
 
 import MDApi from 'utils/MDApi'
 
@@ -18,13 +24,18 @@ const styles = {
 }
 
 
-export default class Headings extends Component {
+export default class Places extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
+      isPlacesViewModalVisible: false,
+      placesViewModalTitle: null,
       places: [],
     }
+
+    this.openPlacesViewModal = this.openPlacesViewModal.bind(this)
+    this.closePlacesViewModal = this.closePlacesViewModal.bind(this)
   }
 
   componentDidMount() {
@@ -38,14 +49,68 @@ export default class Headings extends Component {
         })
       })
   }
+
+  openPlacesViewModal(title, pId) {
+    this.setState({
+      isPlacesViewModalVisible: true,
+      placeId: pId,
+      placesViewModalTitle: title,
+    })
+  }
+
+  closePlacesViewModal() {
+    this.setState({
+      isPlacesViewModalVisible: false,
+    })
+  }
+
+  afterOpenModal() {}
+
   render() {
     return (
       <div style={styles.root}>
         <GridList cellHeight={180} style={styles.gridList}>
           {this.state.places.map(place => (
-            <GridTile key={place.id} title={place.title} subtitle={place.events_count} />
+            <GridTile
+              key={place.id}
+              title={place.title}
+              subtitle={place.events_count}
+              onClick={() => this.openPlacesViewModal(place.title, place.id)}
+            />
           ))}
         </GridList>
+        <div>
+          <ReactModal
+            isOpen={this.state.isPlacesViewModalVisible}
+            contentLabel='Minimal Modal Example'
+            onAfterOpen={this.afterOpenModal}
+            shouldCloseOnOverlayClick={false}
+            style={{
+              overlay: {
+                zIndex: 1200,
+              },
+              content: {
+                border: 'none',
+                borderRadius: 0,
+                padding: 0,
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+              },
+            }}
+          >
+            <AppBar
+              title={this.state.placesViewModalTitle}
+              iconElementLeft={
+                <IconButton>
+                  <NavigationClose onClick={this.closePlacesViewModal} />
+                </IconButton>
+              }
+            />
+            <PlacesList category={this.state.placeId} />
+          </ReactModal>
+        </div>
       </div>
     )
   }

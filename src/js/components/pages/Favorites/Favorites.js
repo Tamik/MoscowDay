6e1203d8e3e4eea0a9cbf2149afb2158102c13ce
Tamik <hello@Tamik.ru>
2017-08-11 15:01:40 +0300

    :ticket: Добавлены модальное окна в компонент Favorites

--- src/js/components/pages/Favorites/Favorites.js
+++ src/js/components/pages/Favorites/Favorites.js
@@ -5,6 +5,8 @@ import localforage from 'localforage'
 import AppBar from 'material-ui/AppBar'
 import Paper from 'material-ui/Paper'
 import { Card, CardMedia, CardTitle } from 'material-ui/Card'
+import { Modal } from 'components/modals'
+
 const FavoritesStore = localforage.createInstance({
   name: 'Favorites',
 })
@@ -14,18 +16,55 @@ const style = {
   margin: 10,
 }
 
-const Favorites = () => (
-  <div>
-    <AppBar title='Избранное' showMenuIconButton={false} />
-    <Paper style={style} zDepth={1}>
-      <Card>
-        <CardMedia>
-          <img src='//placehold.it/256x256' alt='' />
-        </CardMedia>
-        <CardTitle title='Hello, world!' subtitle='Hello, subworld!' />
-      </Card>
-    </Paper>
-  </div>
-)
+export default class Favorites extends Component {
+  state = {
+    id: null,
+    isModalVisible: false,
+    favorites: [],
+    modalTitle: null,
+    payload: {},
+  }
 
-export default Favorites
+  handleOpenModal = (id, title, payload) => {
+    this.setState({
+      id: id,
+      isModalVisible: true,
+      modalTitle: title,
+      payload: payload,
+    })
+  }
+
+  handleCloseModal = () => {
+    this.setState({
+      isModalVisible: false,
+    })
+  }
+  render() {
+    return (
+      <div>
+        <AppBar title='Избранное' showMenuIconButton={false} />
+        <Paper style={style} zDepth={1}>
+          {this.state.favorites.map(event => (
+            <Card
+              key={event.id}
+            >
+              <CardTitle title={event.title} subtitle={event.location_title} />
+            </Card>
+          ))}
+        </Paper>
+        <Modal
+          isOpen={this.state.isModalVisible}
+          title={this.state.modalTitle || ''}
+          content={
+            <div style={{ margin: 16 }}>
+              <h1 style={{ marginBottom: 2 }}>{this.state.payload.title}</h1>
+              <span style={{ display: 'block', marginBottom: 16, opacity: 0.25 }}>{this.state.payload.location_title}</span>
+              <p style={{ marginBottom: 16 }}>{this.state.payload.description}</p>
+            </div>
+          }
+          close={this.handleCloseModal}
+        />
+      </div>
+    )
+  }
+}

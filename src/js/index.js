5bad8d0cd9c08bcae8b80cbc2a8d012a1ad4d4ba
Tamik <hello@Tamik.ru>
2017-08-23 00:55:46 +0300

    Реализована функция генерации GUID

--- src/js/index.js
+++ src/js/index.js
@@ -21,6 +21,14 @@ function renderApp(Component) {
   )
 }
 
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1)
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4()
+}
 function onDeviceReady() {
   const config = {
     AppMetrica: {

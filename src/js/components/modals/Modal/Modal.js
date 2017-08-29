8433654aff7e184ce20e54ada1baec06cca00589
Tamik <hello@Tamik.ru>
2017-08-29 18:13:04 +0300

    Modal: небольшой рефакторинг

--- src/js/components/modals/Modal/Modal.js
+++ src/js/components/modals/Modal/Modal.js
@@ -1,54 +1,56 @@
 import React from 'react'
 import ReactModal from 'react-modal'
 import PropTypes from 'prop-types'
-import { EventInfo } from 'atoms'
 
 import { TopBar } from 'molecules'
 
+// const onScroll = (e) => {
+//   console.log(e)
+// }
 
-const onScroll = (e) => {
-  console.log(e)
-}
-
-const Modal = props => {
-  return (
-    <div>
-      <ReactModal
-        isOpen={props.isOpen}
-        contentLabel={props.title || ''}
-        shouldCloseOnOverlayClick={false}
-        style={{
-          overlay: {
-            zIndex: 1200,
-            overflowY: 'hidden',
-          },
-          content: {
-            border: 'none',
-            borderRadius: 0,
-            padding: 0,
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            display: 'flex',
-            flexDirection: 'column',
-            flexShrink: 1,
-            overflowY: 'hidden',
-          },
-        }}
-      >
-        <TopBar
-        isVisible = {props.isVisibleTopBar}
-        close = {props.close}
-        title = {props.title}
+const Modal = props => (
+  <div>
+    <ReactModal
+      isOpen={props.isOpen}
+      contentLabel={props.title || ''}
+      shouldCloseOnOverlayClick={false}
+      style={{
+        overlay: {
+          overflowY: 'hidden',
+          zIndex: 1200,
+        },
+        content: {
+          display: 'flex',
+          flexDirection: 'column',
+          flexShrink: 1,
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          border: 'none',
+          borderRadius: 0,
+          padding: 0,
+          overflowY: 'hidden',
+        },
+      }}
+    >
+      <TopBar
+        isVisible={props.isVisibleTopBar}
+        close={props.close}
+        title={props.title}
       />
-      <div style={{ overflowY:'auto', flex: 1}} className='modal-content-wrap'>
+      <div
+        style={{
+          flex: 1,
+          overflowY: 'auto',
+        }}
+        className='modal-content-wrap'
+      >
         {props.content}
       </div>
-      </ReactModal>
-    </div>
-  )
-}
+    </ReactModal>
+  </div>
+)
 
 Modal.propTypes = {
   title: PropTypes.string,
